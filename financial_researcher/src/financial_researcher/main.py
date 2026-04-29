bef511b782b893d457bc4bf545eb012088fa370c
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-04-29 03:46:23 +0300

    Add main.py: Entry point for running the financial researcher crew.

---
+++ financial_researcher/src/financial_researcher/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from financial_researcher.crew import FinancialResearcher
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        "company": "Tesla"
+    }
+
+    try:
+        result = FinancialResearcher().crew().kickoff(inputs=inputs)
+        
+        # Print the result
+        print("\n\n=== FINAL REPORT ===\n\n")
+        print(result.raw)
+        
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+if __name__ == "__main__":
+  run()
