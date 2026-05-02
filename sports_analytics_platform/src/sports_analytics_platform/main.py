4d1f926c39a9092466ee7ea69c1e0268bd79677f
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-05-02 17:26:10 +0300

    Init agentops session to track all agent & tasks.

--- sports_analytics_platform/src/sports_analytics_platform/main.py
+++ sports_analytics_platform/src/sports_analytics_platform/main.py
@@ -1,14 +1,22 @@
 from sports_analytics_platform.crew import SportsAnalyticsPlatform
+from dotenv import load_dotenv
+import agentops
+import os
 
+load_dotenv()
 
 def run():
-    inputs = {
+  
+  agentops.init(os.getenv("AGENTOPS_API_KEY"))
+  
+  inputs = {
         "user_question": "Write a full report about everything that happened in the top 5 European leagues recently"
     }
     
-    result = SportsAnalyticsPlatform().crew().kickoff(inputs=inputs)
-    print(result)
+  result = SportsAnalyticsPlatform().crew().kickoff(inputs=inputs)
+  print(result)
 
+  agentops.end_session("Success")
 
 if __name__ == "__main__":
     run()
